37e954e1df38fdac4a5a4b405c0efdd61716a4c9
seul lee <blacksangi@gmail.com>
2018-08-03 15:19:43 +0900

    add pytest and migrations files

--- internationalization/internationalization/settings.py
+++ internationalization/internationalization/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'translation',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'internationalization.urls'
--- internationalization/translation/models.py
+++ internationalization/translation/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# coding: utf-8
 
-# Create your models here.
+
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class HelloText(models.Model):
+    text = models.CharField(max_length=100, verbose_name=_('hello'))
+
+    def __str__(self):
+        return self.text
